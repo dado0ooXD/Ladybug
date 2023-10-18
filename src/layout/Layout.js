a80c24b7155b806350ef1f12fee8f091418307e1
davudxD <carovacdavud6@gmail.com>
2023-10-18 22:25:11 +0200

    layout 1/3

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,27 +1,51 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import React from "react";
 
 const Layout = ({ children }) => {
+  const isXsScreen = useMediaQuery("(max-width:600px)");
   return (
     <Grid container sx={{ display: "flex", justifyContent: "center" }}>
+      {!isXsScreen && (
+        <Grid
+          item
+          xl={3.5}
+          lg={3.5}
+          md={2}
+          sm={1}
+          xs={0}
+          style={{
+            background: "lightgray",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          1
+        </Grid>
+      )}
       <Grid
         item
-        xl={4}
-        lg={4}
-        style={{
-          background: "lightgray",
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
+        xl={5}
+        lg={5}
+        md={6}
+        sm={7}
+        xs={12}
+        style={{ height: "100vh" }}
       >
-        1
-      </Grid>
-      <Grid item xl={4} lg={4} style={{ height: "100vh" }}>
         {children}
       </Grid>
-      <Grid item xl={4} lg={4} style={{ background: "lightgray" }}>
-        3
-      </Grid>
+      {!isXsScreen && (
+        <Grid
+          item
+          xl={3.5}
+          lg={3.5}
+          md={4}
+          sm={4}
+          xs={0}
+          style={{ background: "lightgray" }}
+        >
+          3
+        </Grid>
+      )}
     </Grid>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,105 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import Layout from "../../layout/Layout";
 import ladybug from "../../assets/ladybug.png";
+import CollectionsIcon from "@mui/icons-material/Collections";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import "./Home.css";
 
 const Home = () => {
   return (
     <Layout>
       <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        <Box sx={{ backgroundColor: "pink", height: "60px" }}>
-          <Box>Home</Box>
-          <Box></Box>
-        </Box>
-        <Box sx={{ backgroundColor: "gray", height: "150px" }}>
-          <img
-            src={ladybug}
-            alt="ladybug-logo"
-            style={{ height: "40px", width: "45px" }}
-          />
+        <Box
+          sx={{
+            height: "60px",
+            display: "flex",
+            alignItems: "center",
+            // borderBottom: "1px solid rgb(243 244 246)",
+          }}
+        >
           <Box>
-            <textarea></textarea>
-            <Box></Box>
+            <Typography
+              variant="h6"
+              fontWeight="bold"
+              style={{ marginLeft: "10px" }}
+            >
+              Home
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            height: "140px",
+            display: "flex",
+            borderTop: "1px solid rgb(243 244 246)",
+            borderBottom: "1px solid rgb(243 244 246)",
+          }}
+        >
+          <Box
+            sx={{
+              flex: 1,
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "5px",
+              padding: "4px",
+            }}
+          >
+            <img
+              src={ladybug}
+              alt="ladybug-logo"
+              style={{ height: "35px", width: "45px" }}
+            />
+          </Box>
+          <Box
+            sx={{
+              flex: 8,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              marginLeft: "5px",
+            }}
+          >
+            <textarea
+              style={{
+                outline: "none",
+                resize: "none",
+                height: "60px",
+                border: "none",
+                background: "transparent",
+                borderBottom: "1px solid rgb(243 244 246)",
+                marginTop: "10px",
+              }}
+              placeholder="What's happening?!"
+            ></textarea>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: "10px",
+              }}
+            >
+              <Box>
+                <CollectionsIcon style={{ color: " #FA8072", fontSize: 23 }} />
+                <SignalCellularAltIcon
+                  style={{ color: " #FA8072", marginLeft: "5px", fontSize: 23 }}
+                />
+                <SentimentSatisfiedAltIcon
+                  style={{ color: " #FA8072", marginLeft: "5px", fontSize: 23 }}
+                />
+                <EventNoteIcon
+                  style={{ color: " #FA8072", marginLeft: "5px", fontSize: 23 }}
+                />
+                <LocationOnIcon
+                  style={{ color: " #FA8072", marginLeft: "5px", fontSize: 23 }}
+                />
+              </Box>
+              <button className="ladybug-btn">Ladybag</button>
+            </Box>
           </Box>
         </Box>
       </Box>
